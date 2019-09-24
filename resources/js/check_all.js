c22be57e681032ccfb8152f5967e9f274f24965f
Oscar <oscar.monsreal1996@gmail.com>
2019-09-24 13:32:18 -0500

    Modified the check_all.js New function created 'verificar' and mofidied the select_all eventlistener adding an empty array

--- resources/js/check_all.js
+++ resources/js/check_all.js
@@ -1,33 +1,57 @@
 var select_all = document.getElementById('select_all');
 var checkboxes = document.getElementsByClassName('n_inventario');
-var inputHidden = document.getElementById('values');
+var inputHidden = document.getElementById('nIdBienes[]');
 var array = [];
-//Evento para el checkbox de todo. 
-select_all.addEventListener("change", function(e){
-    for(i=0; i<checkboxes.length; i++){
-        //Se agrega en un arreglo todos los objetos de checkbox
-        checkboxes[i].checked = select_all.checked;
-        //Se agrega el valor de cada checkbox en otro arreglo (array)
-        array.push(checkboxes[i].value);
-        //Se inicializa otro arreglo que separa por comas los valores del anterior arreglo
-        var arraySplited = array.join(", ");
-        //Se pasa el valor directo al input "hidden" con los valores separados por coma
-        inputHidden.setAttribute('value', arraySplited);
+
+
+function verificar() {
+    array = [];
+    inputHidden.setAttribute('value', "");
+    for (var i = 0; i < checkboxes.length; i++) {
+        if(checkboxes[i].checked == true){
+            console.log("Hola");
+            array.push(checkboxes[i].value);
+            var arraySplited = array.join(", ");
+            console.log(arraySplited);
+            inputHidden.setAttribute('value', arraySplited);
+        }
+        
     }
-    //console.log(array);
+
+
+    console.log(array);
+}
+
+//Evento para el checkbox de todo. 
+select_all.addEventListener("change", function (e) {
+    array = [];
+    for (i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].checked = select_all.checked;
+        array.push(checkboxes[i].value);
+        var arraySplited = array.join(", ");
+        if (this.checked == true) {
+            inputHidden.setAttribute('value', arraySplited);
+        }
+        if (this.checked == false) {
+            inputHidden.removeAttribute('value', arraySplited);
+            array = [];
+        }
+
+    }
+    console.log(array);
     console.log(arraySplited);
-    
+
 });
 
 /***** Establece la condición del check *****/
-for(var i = 0;i<checkboxes.length; i++){
-    checkboxes[i].addEventListener('change', function(e){
-        if(this.checked == false){
+for (var i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('change', function (e) {
+        if (this.checked == false) {
             select_all.checked = false;
         }
-        if(document.querySelectorAll('.n_inventario:checked').length == checkboxes.length){
+        if (document.querySelectorAll('.n_inventario:checked').length == checkboxes.length) {
             select_all.checked = true;
         }
     });
-    
+
 }
