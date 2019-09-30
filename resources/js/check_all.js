abfbac14397e1eb0cbd291f892596ea6c7ae1671
Oscar <oscar.monsreal1996@gmail.com>
2019-09-30 12:09:07 -0500

    added the Jquery function

--- resources/js/check_all.js
+++ resources/js/check_all.js
@@ -2,9 +2,59 @@ var select_all = document.getElementById('select_all');
 var checkboxes = document.getElementsByClassName('n_inventario');
 var inputHidden = document.getElementById('nIdBienes[]');
 var formulario = document.getElementById('iBienes');
+$(function() {
+    $('input:checkbox.main-checkbox').click(function() {
+        var array = [];
+        var arraysplited = [];
+        var parent = $(this).closest('.main-parent');
+        $(parent).find('.n_inventario').prop("checked", $(this).prop("checked"));
+        if ($(this).is(':checked')) {
+            $(parent).find('.n_inventario:checked').each(function() {
+                arraysplited = array.push($(this).val().split(','));
+                $(parent).parent().parent().parent().children('.t_9').children('.i_b')
+                    .val(
+                        array).css({
+                        "color": "green",
+                        "border": "2px solid green"
+                    });
 
+                //array.push($(this).val());
+
+            });
+        } else {
+            $(parent).parent().parent().parent().children('.t_9').children('.i_b')
+                .val(
+                    "").css({
+                    "color": "red",
+                    "border": "2px solid red"
+                });
+        }
+
+    });
+});
+
+$('.nInventarios').removeClass('nInventarios');
+$('.n_inventario').change(function(e) {
+    valores = [];
+    valor = $(this).parent().parent().parent().parent().parent().parent().parent().children('.t_9').children('.i_b').val();
+    valores = (valor != '') ? valor.split(',') : [];
+    v = $(this).val();
+    if ($(this).is(':checked')) {
+        if ($.inArray(v, valores) == -1) {
+            valores.push(v);
+        }
+        $(this).parent().parent().parent().parent().parent().parent().parent().children('.t_9').children('.i_b').val(valores.toString());
+    } else {
+        //valores.splice($.inArray(v, val),1);
+        valor = valor.replace(v + ',', '');
+        valor = valor.replace(',' + v, '');
+        valor = valor.replace(v, '');
+        $(this).parent().parent().parent().parent().parent().parent().parent().children('.t_9').children('.i_b').val(valor);
+    }
+});
 
 /**** MÉTODO BUENO *** */
+/*
 formulario.addEventListener("click", function () {
     array = [];
     for (var i = 0; i < checkboxes.length; i++) {
@@ -18,9 +68,10 @@ formulario.addEventListener("click", function () {
 
 });
 
-
+*/
 //Evento para el checkbox de todo. 
 /* FUNCIONA PARA UNO CUANDO ES getById*/
+/*
 select_all.addEventListener("change", function (e) {
     array = [];
     for (i = 0; i < checkboxes.length; i++) {
@@ -42,10 +93,11 @@ select_all.addEventListener("change", function (e) {
 
 });
 
-
+*/
 
 
 /***** Establece la condición del check *****/
+/*
 for (var i = 0; i < checkboxes.length; i++) {
     checkboxes[i].addEventListener('change', function (e) {
         if (this.checked == false) {
@@ -56,4 +108,4 @@ for (var i = 0; i < checkboxes.length; i++) {
         }
     });
 
-}
+}*/
